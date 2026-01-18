4c44b7c91e1778c17e58bcdd359244aeb958bbc4
zaid242003 <mohammadzbt69@gmail.com>
2026-01-18 13:39:10 +0500

    Changes made with intentionally wrong testing

--- domain/RoomTest.java
+++ domain/RoomTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoomTest {
 
     @Test
-    void shouldCreateValidRoom() {
+    void shouldCreateRoomWithValidNumber() {
         Room room = new Room(101);
         assertFalse(room.isOccupied());
     }
@@ -15,7 +15,9 @@ class RoomTest {
     void shouldAssignGuestToRoom() {
         Room room = new Room(102);
         Guest guest = new Guest("Ali", "Lahore");
+
         room.createGuest(guest);
+
         assertTrue(room.isOccupied());
     }
 
@@ -23,4 +25,14 @@ class RoomTest {
     void shouldRejectInvalidRoomNumber() {
         assertThrows(IllegalArgumentException.class, () -> new Room(0));
     }
+
+    @Test
+    void intentionallyFailingTest_missingException() {
+    Room room = new Room(10);
+    assertThrows(IllegalArgumentException.class, () -> {
+        room.createGuest(new Guest("Ali", "Lahore"));
+    });
 }
+
+}
+
